e5540302d0da443b493d812ca369cce9452cfdfe
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2025-03-07 19:57:32 -0500

    Add styles to the button component

--- src/components/boton.jsx
+++ src/components/boton.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */ //Desactivar validación de tipos de props
+import '../stylesheets/boton.css'
 
 // Boton({texto}) => sintaxis de desestructuración
 function Boton({ texto, botonDeClick, manejarClick }){
     return (
         <button
           // Decide si el botón se mostrará como de click o de reinicio
+          // Asigna una clase a su botón respectivo
           className={botonDeClick ? 'boton-click' : 'boton-reiniciar'}
           // Se llama a manejar clic cuando ocurra el evento, manejarclick es una fx que se pasa como prop
           // Manejar click se define en App.jsx
